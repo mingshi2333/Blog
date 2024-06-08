e94468208c2cd9360cf8f8ebf46d666148c899a2
mingshi2333 <akashi_asuka@outlook.com>
2024-06-08 16:48:43 +0300

    1

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -2,18 +2,11 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const enSidebar = sidebar({
   "/": [
-    "",
     {
       text: "Articles",
       icon: "book",
       prefix: "posts/",
       children: "structure",
-    },
-    "intro",
-    {
-      text: "Slides",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/guide/content/revealjs/demo.html",
     },
   ],
 });
